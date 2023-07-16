270e7baa73e5886677c178d35a034e9562606a40
amaankhan67 <139677672+amaankhan67@users.noreply.github.com>
2023-07-17 01:54:27 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -8,9 +8,15 @@ let addtitle=document.getElementById("addtitle");
 addtitle.style.color='green';
 addtitle.style.fontWeight='bold';
 
-var items=document.getElementsByClassName("list-group-item");
-items[3].style.backgroundColor='green';
+// var items=document.getElementsByClassName("list-group-item");
+// items[3].style.backgroundColor='green';
 
+// for( let a of items)
+// {
+//     a.style.fontWeight="bold";
+// }
+
+var li=document.getElementsByTagName('li');
 for( let a of items)
 {
     a.style.fontWeight="bold";

706a162e7dea97cf8da1102880a8cd6977247955
amaankhan67 <139677672+amaankhan67@users.noreply.github.com>
2023-07-16 16:46:24 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,9 @@
+let title=document.getElementById("title");
+title.textContent="Hello";
+
+let header=document.getElementById("main-header");
+header.style.border='solid 10px #000';
+
+let addtitle=document.getElementById("addtitle");
+addtitle.style.color='green';
+addtitle.style.fontWeight='bold';

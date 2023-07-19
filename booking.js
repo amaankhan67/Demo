93cf1bc374a52b361c66bcfa6f7ab7f045fe0f86
amaankhan67 <139677672+amaankhan67@users.noreply.github.com>
2023-07-20 00:08:30 +0530

    Update booking.js

--- booking.js
+++ booking.js
@@ -8,11 +8,40 @@ form.addEventListener('submit',(e)=>{
 var i=1;
 
 submitBtn.addEventListener('click',(e)=>{
+    var name=document.getElementById("name").value;
+    var email=document.getElementById("email").value;
+    var date=document.getElementById("date").value;
     var Obj={
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        date: document.getElementById("date").value
+        name: name,
+        email: email,
+        date: date
     };
 
     localStorage.setItem(`Obj${i++}`, JSON.stringify(Obj));
+    //NEW LIST
+    var newList=document.createElement('li');
+    newList.setAttribute('class','list-group-item');
+    var newText=document.createTextNode(name+" "+email+" "+date);
+    newList.appendChild(newText);
+
+    //NEW DELETE BUTTON
+    var newButton=document.createElement('button');
+    newButton.setAttribute("class","btn btn-danger btn-sm float-right");
+    var newTextButton=document.createTextNode('Delete');
+    newButton.appendChild(newTextButton);
+
+
+    var parent=document.getElementById("items");
+    parent.appendChild(newList);
+    parent.appendChild(newButton);
+});
+
+//delete Button
+var itemList=document.getElementById("items");
+itemList.addEventListener('click',(e)=>{
+        if(confirm('Are You Sure?')){
+          var li = e.target.parentElement;
+          localStorage.removeItem(`Obj${Array.from(li).indexOf(li)}`);
+          itemList.removeChild(li);
+        }
 });

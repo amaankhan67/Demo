8269c87ad024bc8d49a0d55af7605bfd082fa3a4
amaankhan67 <139677672+amaankhan67@users.noreply.github.com>
2023-08-24 23:42:05 +0530

    Sending Response

--- Test.js
+++ Test.js
@@ -1,7 +1,12 @@
 const http =require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log("Amaan");
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>Home Page</title></head>');
+    res.write('<body>WELCOME TO MY NODE JS PROJECT!</body>')
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(4000);
